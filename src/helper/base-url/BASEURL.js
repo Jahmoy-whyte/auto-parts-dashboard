2c57e2b08c8821ebb68409febe8d66bf5d15d717
johnwata <divebackup152@gmail.com>
2023-10-17 12:23:01 -0600

    changed base url to prod

--- src/helper/base-url/BASEURL.js
+++ src/helper/base-url/BASEURL.js
@@ -1,3 +1,3 @@
-//export const BASE_URL = "https://auto-parts-server-2lng.onrender.com";
+export const BASE_URL = "https://auto-parts-server-2lng.onrender.com";
 
-export const BASE_URL = "http://localhost:3000";
+//export const BASE_URL = "http://localhost:3000";
